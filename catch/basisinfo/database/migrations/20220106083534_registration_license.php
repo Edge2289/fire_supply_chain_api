d7a45565fc607b37f97f87d544743dc85fc9b30c
Edge2289 <1131191695@qq.com>
2022-01-10 14:35:40 +0800

    调整迁移文件

--- catch/basisinfo/database/migrations/20220106083534_registration_license.php
+++ catch/basisinfo/database/migrations/20220106083534_registration_license.php
@@ -19,15 +19,16 @@ class RegistrationLicense extends Migrator
     {
         $table = $this->table('registration_license', ['engine' => 'Myisam', 'collation' => 'utf8mb4_general_ci', 'comment' => '备案凭证', 'id' => 'id', 'signed' => true, 'primary_key' => ['id']]);
         $table->addColumn('business_license_id', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false, 'default' => 0, 'signed' => true, 'comment' => '营业执照id',])
-            ->addColumn('registration_license_code', 'string', ['limit' => 1000, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '备案号',])
-            ->addColumn('registration_date', 'string', ['limit' => 1000, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '备案日期',])
-            ->addColumn('operation_mode', 'string', ['limit' => 100, 'null' => false, 'default' => '1', 'signed' => false, 'comment' => '经营方式 {1:批发,2::零售.3:批发兼零售}',])
-            ->addColumn('legal_person', 'string', ['limit' => 1000, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '法人',])
-            ->addColumn('incharge_person', 'string', ['limit' => 1000, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '企业负责人',])
-            ->addColumn('premise', 'string', ['limit' => 1000, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '住所',])
-            ->addColumn('residence', 'string', ['limit' => 1000, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '经营场所',])
-            ->addColumn('warehouse_address', 'string', ['limit' => 1000, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '库房地址',])
-            ->addColumn('equipment_class', 'string', ['limit' => 1000, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '医疗分类 逗号分割',])
+            ->addColumn('registration_license_code', 'string', ['limit' => 100, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '备案号',])
+            ->addColumn('registration_date', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '备案日期',])
+            ->addColumn('operation_mode', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'null' => false, 'default' => '1', 'signed' => false, 'comment' => '经营方式 {1:批发,2::零售.3:批发兼零售}',])
+            ->addColumn('company_name', 'string', ['limit' => 100, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '企业名称',])
+            ->addColumn('legal_person', 'string', ['limit' => 60, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '法人',])
+            ->addColumn('incharge_person', 'string', ['limit' => 60, 'null' => false, 'default' => "", 'signed' => true, 'comment' => '企业负责人',])
+            ->addColumn('premise', 'string', ['limit' => 300, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '住所',])
+            ->addColumn('residence', 'string', ['limit' => 300, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '经营场所',])
+            ->addColumn('warehouse_address', 'string', ['limit' => 300, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '库房地址',])
+            ->addColumn('equipment_class', 'string', ['limit' => 300, 'null' => false, 'default' => '', 'signed' => false, 'comment' => '医疗分类 逗号分割',])
 
 
             ->addColumn('created_at', 'integer', ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false, 'default' => 0, 'signed' => true, 'comment' => '创建时间',])
