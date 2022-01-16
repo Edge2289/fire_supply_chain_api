5ef035c31099b6156c9a5abc0bee6b81a7db8acb
Edge2289 <1131191695@qq.com>
2022-01-16 17:07:40 +0800

    调整

--- catch/basisinfo/database/migrations/20220115131007_product_basic_info.php
+++ catch/basisinfo/database/migrations/20220115131007_product_basic_info.php
@@ -22,9 +22,9 @@ class ProductBasicInfo extends Migrator
     {
         $table = $this->table('product_basic_info', ['engine' => 'Myisam', 'collation' => 'utf8mb4_general_ci', 'comment' => '产品基础信息', 'id' => 'id', 'signed' => true, 'primary_key' => ['id']]);
         $table->addColumn('product_name', 'string', ['limit' => 300, 'null' => false, 'default' => '', 'signed' => true, 'comment' => '产品名称',])
-            ->addColumn('product_img', 'string', ['limit' => 200, 'default' => '', 'signed' => false, 'comment' => '产品实物图片',])
-            ->addColumn('storage_conditions', 'string', ['limit' => 100, 'default' => '', 'signed' => true, 'comment' => '储运条件',])
-            ->addColumn('data_maintenance', 'string', ['limit' => 30, 'default' => "", 'signed' => false, 'comment' => '资料维护 1 注册证 2 备案凭证',])
+            ->addColumn('product_img', 'string', ['limit' => 200, 'default' => '', 'null' => false, 'signed' => false, 'comment' => '产品实物图片',])
+            ->addColumn('storage_conditions', 'string', ['limit' => 100, 'null' => false, 'default' => '', 'signed' => true, 'comment' => '储运条件',])
+            ->addColumn('data_maintenance', 'string', ['limit' => 30, 'default' => "", 'null' => false, 'signed' => false, 'comment' => '资料维护 1 注册证 2 备案凭证',])
             ->addColumn('audit_status', 'integer', ['limit' => MysqlAdapter::INT_TINY, 'default' => 0, 'signed' => true, 'comment' => '审核状态 {0:未审核,1:已审核,2:审核失败}',])
             ->addColumn('audit_info', 'string', ['limit' => 300, 'default' => '', 'signed' => true, 'comment' => '审核信息',])
             ->addColumn('audit_user_id', 'integer', ['limit' => MysqlAdapter::INT_MEDIUM, 'default' => 0, 'signed' => true, 'comment' => '审核人id',])
