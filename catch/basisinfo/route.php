9851a2a4df2fa54074e7e951d7c0ba6def012457
Edge2289 <1131191695@qq.com>
2022-01-25 10:18:54 +0800

    调整路由问题

--- catch/basisinfo/route.php
+++ catch/basisinfo/route.php
@@ -23,9 +23,9 @@ $router->group(function () use ($router){
     // 厂家路由
     $router->group(function () use ($router) {
         $router->get('factory', '\catchAdmin\basisinfo\controller\Factory@index'); // 厂家
-        $router->post('factory', '\catchAdmin\basisinfo\controller\Factory@save'); // 厂家
-        $router->put('factory/<id>', '\catchAdmin\basisinfo\controller\Factory@update'); // 厂家
-        $router->post('factory/audio/<id>', '\catchAdmin\basisinfo\controller\Factory@update'); // 厂家
+        $router->post('factory', '\catchAdmin\basisinfo\controller\Factory@save'); // 厂家保存
+        $router->put('factory/<id>', '\catchAdmin\basisinfo\controller\Factory@update'); // 厂家更新
+        $router->post('factory/audio/<id>', '\catchAdmin\basisinfo\controller\Factory@update'); // 厂家审核
 
     });
     // 购货者路由
