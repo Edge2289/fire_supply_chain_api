2562cf6dc4c7c5cc370c2e4d3b9a49a898a8d70f
Edge2289 <1131191695@qq.com>
2022-02-06 21:01:07 +0800

    添加

--- catch/purchase/controller/PurchaseOrder.php
+++ catch/purchase/controller/PurchaseOrder.php
@@ -11,6 +11,8 @@ namespace catchAdmin\purchase\controller;
 
 
 use app\Request;
+use catchAdmin\purchase\model\PurchaseOrderDetails;
+use catchAdmin\purchase\request\PurchaseOrderRequest;
 use catcher\base\CatchController;
 use catchAdmin\purchase\model\PurchaseOrder as PurchaseOrderModel;
 use catcher\CatchResponse;
@@ -23,13 +25,15 @@ use catcher\CatchResponse;
 class PurchaseOrder extends CatchController
 {
     protected $purchaseOrderModel;
+    protected $purchaseOrderDetailsModel;
 
     public function __construct(
-        PurchaseOrderModel $purchaseOrderModel
+        PurchaseOrderModel $purchaseOrderModel,
+        PurchaseOrderDetails $purchaseOrderDetailsModel
     )
     {
-        // 构造函数
         $this->purchaseOrderModel = $purchaseOrderModel;
+        $this->purchaseOrderDetailsModel = $purchaseOrderDetailsModel;
     }
 
     // 列表
@@ -38,30 +42,154 @@ class PurchaseOrder extends CatchController
         return CatchResponse::paginate($this->purchaseOrderModel->getList());
     }
 
-    // 保存
+    /**
+     * 保存
+     *
+     * @author xiejiaqing
+     * @param Request $request
+     * @return \think\response\Json
+     */
     public function save(Request $request)
     {
         // 保存基础信息
         $params = $request->param();
+        $this->validator(PurchaseOrderRequest::class, $params);
         // 保存商品
         $goodsDetails = $params['goods_details'];
+        unset($params['goods_details']);
+        $params['purchase_code'] = getCode("PO");
+        $this->purchaseOrderModel->startTrans();
+        try {
+            $purchaseMap = [];
+            $id = $this->purchaseOrderModel->createBy($purchaseMap);
+            if (empty($id)) {
+                throw new \Exception("采购订单添加失败");
+            }
+            foreach ($goodsDetails as &$goodsDetail) {
+                $goodsDetail['purchase_order_id'] = $id;
+            }
+            $gId = $this->purchaseOrderDetailsModel->insertAll($goodsDetails);
+            if (empty($gId)) {
+                throw new \Exception("采购订单商品添加失败");
+            }
+            // 提交事务
+            $this->purchaseOrderModel->commit();
+        } catch (\Exception $exception) {
+            // 回滚事务
+            $this->purchaseOrderModel->rollback();
+            return CatchResponse::fail($exception->getMessage());
+        }
+        return CatchResponse::success();
     }
 
-    // 更新
-    public function update()
+    /**
+     * 更新
+     *
+     * @author xiejiaqing
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function update(Request $request)
     {
+        // 保存基础信息
+        $params = $request->param();
+        $this->validator(PurchaseOrderRequest::class, $params);
+        if (empty($params['id'])) {
+            return CatchResponse::fail("更新缺失主键id");
+        }
+        // 保存商品
+        $goodsDetails = $params['goods_details'];
 
+        $this->purchaseOrderModel->startTrans();
+        // 添加事务 排他锁
+        $purchaseOrderModel = $this->purchaseOrderModel->where("id", $params['id'])->lock(true)->find();
+        if (empty($purchaseOrderModel)) {
+            return CatchResponse::fail("不存在当前数据");
+        }
+        // 审核成功不可以修改
+        if ($purchaseOrderModel['audit_status'] == 1) {
+            return CatchResponse::fail("采购订单已审核,无法修改");
+        }
+        if ($purchaseOrderModel['status'] == 1) {
+            return CatchResponse::fail("采购订单已完成,无法修改");
+        }
+        try {
+
+            $b = $this->purchaseOrderDetailsModel->where("product_id", $params['id'])->delete();
+            if (!$b) {
+                throw new \Exception("清除采购订单商品失败");
+            }
+            $pb = $this->purchaseOrderModel->updateBy($params['id'], $params);
+            if (!$pb) {
+                throw new \Exception("修改采购订单失败");
+            }
+            // 重新添加商品数据
+            foreach ($goodsDetails as &$goodsDetail) {
+                $goodsDetail['purchase_order_id'] = $params['id'];
+            }
+            $gId = $this->purchaseOrderDetailsModel->insertAll($goodsDetails);
+            if (empty($gId)) {
+                throw new \Exception("采购订单商品添加失败");
+            }
+            // 清除采购订单的商品数据
+            $this->purchaseOrderModel->commit();
+        } catch (\Exception $exception) {
+            $this->purchaseOrderModel->rollback();
+            return CatchResponse::fail($exception->getMessage());
+        }
+        return CatchResponse::success();
     }
 
-    // 审核
-    public function audit()
+    /**
+     * 审核
+     *
+     * @author xiejiaqing
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function audit(Request $request)
     {
-
-    }
-
-    // 取消
-    public function cancel()
-    {
-
+        // 保存基础信息
+        $params = $request->param();
+        $this->validator(PurchaseOrderRequest::class, $params);
+        if (empty($params['id'])) {
+            return CatchResponse::fail("更新缺失主键id");
+        }
+        $this->purchaseOrderModel->startTrans();
+        // 添加事务 排他锁
+        $purchaseOrderModel = $this->purchaseOrderModel->where("id", $params['id'])->lock(true)->find();
+        if (empty($purchaseOrderModel)) {
+            return CatchResponse::fail("不存在当前数据");
+        }
+        // 审核成功不可以修改
+        if ($purchaseOrderModel['audit_status'] == 1) {
+            return CatchResponse::fail("采购订单已审核,无法修改");
+        }
+        if ($purchaseOrderModel['status'] == 1) {
+            return CatchResponse::fail("采购订单已完成,无法修改");
+        }
+        // 更新
+        try {
+            $b = $this->purchaseOrderDetailsModel->updateBy($params['id'], [
+                'audit_status' => $params['audit_status'],
+                'audit_info' => $params['audit_info'],
+                'audit_user_id' => request()->user()->id,
+                'audit_user_name' => request()->user()->username,
+            ]);
+            if (!$b) {
+                throw new \Exception("清除采购订单商品失败");
+            }
+            $this->purchaseOrderModel->commit();
+        } catch (\Exception $exception) {
+            $this->purchaseOrderModel->rollback();
+            return CatchResponse::fail($exception->getMessage());
+        }
+        return CatchResponse::success();
     }
 }
--- catch/purchase/model/PurchaseOrderDetails.php
+++ catch/purchase/model/PurchaseOrderDetails.php
@@ -10,11 +10,13 @@
 namespace catchAdmin\purchase\model;
 
 
+use catcher\base\CatchModel;
+
 /**
  * Class PurchaseOrderDetails
  * @package catchAdmin\purchase\model
  */
-class PurchaseOrderDetails
+class PurchaseOrderDetails extends CatchModel
 {
     // 这个根据登陆的账号去获取链接
     protected $connection = 'business';
