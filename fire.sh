667d00da4848ecb4bf839bd6e6bc38d0b7882cad
Edge2289 <1131191695@qq.com>
2022-03-28 11:14:33 +0800

    添加

---
+++ fire.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env
+
+php think catch-migrate:run basisinfo
+php think catch-migrate:run finacial
+php think catch-migrate:run inventory
+php think catch-migrate:run login
+php think catch-migrate:run monitor
+php think catch-migrate:run permissions
+php think catch-migrate:run purchase
+php think catch-migrate:run salesManage
+php think catch-migrate:run system
